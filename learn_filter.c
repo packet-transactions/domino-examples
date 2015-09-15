03939d7b27f16a04f2aec78b106db9bc434f9252
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-09-15 13:47:56 -0700

    Revert "Currently, we can only implement a bloom filter with k=1 hash function"
    This reverts commit 7b1b90cd844993d2d848b1fa5f0541a1d050e03f.
    Turns out I was wrong. This is really a bloom filter of size 256 (only
    instead of all hash functions hitting the same memory, each hash
    function hits its own shadow copy and the results are ored together
    after the fact.). Thanks to Mihai for pointing this out. This was so
    obvious in retrospect, I feel dumb!

--- learn_filter.c
+++ learn_filter.c
@@ -4,16 +4,26 @@ struct Packet {
   int sport;
   int dport;
   int member;
-  int filter_idx;
+  int filter1_idx;
+  int filter2_idx;
+  int filter3_idx;
 };
 
 #define NUM_ENTRIES 256
 
-int filter[NUM_ENTRIES] = {0};
+int filter1[NUM_ENTRIES] = {0};
+int filter2[NUM_ENTRIES] = {0};
+int filter3[NUM_ENTRIES] = {0};
 
 void func(struct Packet pkt) {
-  pkt.filter_idx = hash2(pkt.sport, pkt.dport) % NUM_ENTRIES;
-  pkt.member = (filter[pkt.filter_idx]);
+  pkt.filter1_idx = hash2(pkt.sport, pkt.dport) % NUM_ENTRIES;
+  pkt.filter2_idx = hash2(pkt.sport, pkt.dport) % NUM_ENTRIES;
+  pkt.filter3_idx = hash2(pkt.sport, pkt.dport) % NUM_ENTRIES;
+  pkt.member = (filter1[pkt.filter1_idx] &&
+                filter2[pkt.filter2_idx] &&
+                filter3[pkt.filter3_idx]);
 
-  filter[pkt.filter_idx] = 1;
+  filter1[pkt.filter1_idx] = 1;
+  filter2[pkt.filter2_idx] = 1;
+  filter3[pkt.filter3_idx] = 1;
 }
