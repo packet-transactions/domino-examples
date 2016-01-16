059afac32271d40a88466cf2c2ee47bee0ce2a1c
Anirudh Sivaraman <sk.anirudh@gmail.com>
2016-01-16 18:27:19 -0500

    First instance of back tracking!
    RCP doesn't map when we run the SKETCH preprocessor because recomputing
    variables is wasteful for RCP. We get around this by backtracking if
    required.

--- run_expts.py
+++ run_expts.py
@@ -5,6 +5,18 @@ import sys
 import subprocess
 import os
 
+# Check if we can compile Domino program to the atom
+def check_compile(domino_file, env_vars, run_preproc):
+  out,err = program_wrapper(program = ["domino", domino_file, "int_type_checker,desugar_comp_asgn,if_converter,algebra_simplify,array_validator,stateful_flanks,ssa,expr_propagater,expr_flattener,cse,partitioning," + ("sketch_preprocessor," if run_preproc else "") + "sketch_backend"], environment = env_vars)
+  all_codelets_mapped = True;
+  for line in err.splitlines():
+    if "with random seed" in line:
+      if "succeeded" in line:
+        all_codelets_mapped = all_codelets_mapped and True;
+      if "failed" in line:
+        all_codelets_mapped = all_codelets_mapped and False;
+  return all_codelets_mapped;
+
 # Program wrapper
 # Takes a command line of program arguements,
 # executes it, and prints something out whether it succeeds or fails
@@ -34,16 +46,14 @@ for domino_file in open(domino_file_list, "r").read().splitlines():
   for atom_template in open(atom_template_list, "r").read().splitlines():
     env_vars = os.environ.copy()
     env_vars["ATOM_TEMPLATE"] = atom_template
-    out,err = program_wrapper(program = ["domino", domino_file, "int_type_checker,desugar_comp_asgn,if_converter,algebra_simplify,array_validator,stateful_flanks,ssa,expr_propagater,expr_flattener,cse,partitioning,sketch_preprocessor,sketch_backend"], environment = env_vars)
-    count = 0
-    all_codelets_mapped = True;
-    for line in err.splitlines():
-      if "with random seed" in line:
-        count = count + 1
-        if "succeeded" in line:
-          all_codelets_mapped = all_codelets_mapped and True;
-        if "failed" in line:
-          all_codelets_mapped = all_codelets_mapped and False;
+    all_codelets_mapped = False
+    # Run with preprocessor
+    print "Running with preprocessor "
+    all_codelets_mapped = check_compile(domino_file, env_vars, True);
+    # Run without preprocessor
+    if (all_codelets_mapped == False):
+      print "Failed to map, running without preprocessor "
+      all_codelets_mapped = check_compile(domino_file, env_vars, False)
     result_matrix[domino_file][atom_template] = all_codelets_mapped;
 
 # first print out atom templates
