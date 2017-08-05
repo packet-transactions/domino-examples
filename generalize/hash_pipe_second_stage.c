0554afdf986d9dd821ec5309f13b3bbb0771a221
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-08-05 15:10:50 -0400

    Version of HashPipe*'s second stage that actually compiles with pairs
    *This requires us to assume that value being 0 is equivalent to a key not
    existing. Basically if by convention value==0 means key doesn't exist, then
    we can avoid the additional check on key, which is beyond our atoms.

--- generalize/hash_pipe_second_stage.c
+++ generalize/hash_pipe_second_stage.c
@@ -20,20 +20,13 @@ void func(struct Packet p) {
  if (array2key[p.loc] == p.ckey) {     // key already exists
    array2val[p.loc] = array2val[p.loc] + p.cval; // add cval
    p.terminate = 1;
- } else {
-   if (array2key[p.loc] == 0) {  // empty slot
-     array2key[p.loc] = p.ckey;          // init. key
-     array2val[p.loc] = p.cval;          // init. value
-     p.terminate = 1;
-   } else {
-     if (array2val[p.loc] < p.cval) { // compare
-       p.tmpkey = array2key[p.loc];        // swap
-       p.tmpval = array2val[p.loc];
-       array2key[p.loc] = p.ckey;
-       array2val[p.loc] = p.cval;
-       p.ckey = p.tmpkey;
-       p.cval = p.tmpval;
-     }
-   }
+ } else if (array2val[p.loc] < p.cval) {
+   p.tmpkey = array2key[p.loc];        // swap
+   p.tmpval = array2val[p.loc];
+   array2key[p.loc] = p.ckey;
+   array2val[p.loc] = p.cval;
+   p.ckey = p.tmpkey;
+   p.cval = p.tmpval;
+   p.terminate = (array2key[p.loc] == 0);
  }
 }
